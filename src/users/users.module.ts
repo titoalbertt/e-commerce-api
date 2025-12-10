fa582f8ca5917dab061d31b07853daaf2a8e6f55
Tito Albert <albert@titosolutions.ph>
2025-12-10 13:29:43 +0800

    updated user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DbModule } from 'src/db/db.module';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
-@Module({})
+@Module({
+  imports: [DbModule],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
 export class UsersModule {}
